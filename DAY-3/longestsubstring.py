356cee018e29a7530d3c7c0da37e1a90e18276dc
notDEVANG <devangpujare@gmail.com>
2026-08-28 23:37:24 +0530

    DSA: 2026-08-28 23:37

---
+++ DAY-3/longestsubstring.py
@@ -0,0 +1,14 @@
+def longestsub(s):
+    left = 0
+    current_window = set()
+    max_length = 0
+
+    for right in range(len(s)):
+        while s[right] in current_window:
+            current_window.remove(s[left])
+            left += 1
+
+        current_window.add(s[right])
+        max_length = max(max_length, right - left + 1)
+    return max_length
+print(longestsub("abcabcbb"))
