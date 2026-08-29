1e26cd3c959dcce421e321638436338aee2c3d60
notDEVANG <devangpujare@gmail.com>
2026-08-29 22:52:40 +0530

    DSA: 2026-08-29 22:52

--- DAY-3/validparanthesis.py
+++ DAY-3/validparanthesis.py
@@ -9,4 +9,12 @@ def validpara(s):
     for char in s:
         if char in pairs:
             if not stack or stack[-1] != pairs[char]:
-                
+                return False
+            stack.pop()
+
+        else:
+            stack.append(char)
+
+    return True
+
+print(validpara("([)]"))
