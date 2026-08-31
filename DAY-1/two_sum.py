f8c7df492300f6cc8cfa2b290308c92a24237701
notDEVANG <devangpujare@gmail.com>
2026-08-31 23:56:26 +0530

    DSA: 2026-08-31 23:56

--- DAY-1/two_sum.py
+++ DAY-1/two_sum.py
@@ -26,5 +26,5 @@ def two_sum(nums, target):
 
         if complement in seen: # have I seen it already?
             return [seen[complement], i]           # the two indices
-        seen[nums[i]] = i            # store current number so future numbers can find it
+        seen[nums[i]] = i            # store current number so future numbers can find it       
 print(two_sum([2,7,11,15],9))   # problem guarantees a solution, so no return needed after loop
