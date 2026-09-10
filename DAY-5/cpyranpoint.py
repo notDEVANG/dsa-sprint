ff981589490dd961053f21e30d2b11f343be472d
notDEVANG <devangpujare@gmail.com>
2026-09-10 23:57:42 +0530

    DSA: 2026-09-10 23:57

--- DAY-5/cpyranpoint.py
+++ DAY-5/cpyranpoint.py
@@ -1,14 +1,14 @@
-"""
-# Definition for a Node.
+
+#Definition for a Node.
 class Node:
     def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
         self.val = int(x)
         self.next = next
         self.random = random
-"""
+
 
 class Solution:
-    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+    def copyRandomList(self, head):
         old_to_new = {None: None}
 
         current = head
