ca3b7e4c577e4e520debb2c0936a3756895cf8d6
notDEVANG <devangpujare@gmail.com>
2026-09-19 23:55:07 +0530

    DSA: 2026-09-19 23:55

--- DAY-4/binary.py
+++ DAY-4/binary.py
@@ -6,7 +6,7 @@ def search(nums, target):
         mid = (left + right) // 2
         if nums[mid] == target:
             return mid
-        elif nums[mid] < tar:
+        elif nums[mid] < target:
             left = mid + 1
         else:
             right = mid - 1
