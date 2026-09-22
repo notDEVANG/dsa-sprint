e7cd501c6b6148e460abc7b39d623c60626a410f
notDEVANG <devangpujare@gmail.com>
2026-09-22 23:33:05 +0530

    DSA: 2026-09-22 23:33

--- DAY-1/topkfreq.py
+++ DAY-1/topkfreq.py
@@ -1,2 +1,22 @@
 def k_top(nums, k):
-    
+    count = {}
+
+    for num in nums:
+        if num in count:
+            count[num] += 1
+        else:
+            count[num] = 1
+
+    buckets = [[] for _ in range(len(nums) - 1)]
+
+    for num, freq in count.items():
+        buckets[freq].append(num)
+
+    result = []
+
+    for freq in range(len(buckets)):
+        for num in buckets[freq]:
+            result.append(num)
+
+            if len(result) == k:
+                return result
